becc9d8dca73d59fe456a29af257e4449f833251
Shiv Gupta <shiv@Shivs-MacBook-Pro.local>
2018-11-03 19:35:52 -0400

    Making error handling work on front-end

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -11,7 +11,7 @@ class LoginForm extends React.Component {
       password: ""
     },
     loading: false,
-    erros: {}
+    errors: {}
   };
 
   onChange = e =>
@@ -20,13 +20,16 @@ class LoginForm extends React.Component {
     });
 
   onSubmit = () => {
-    const errors = this.validate(this.state.date);
+    const errors = this.validate(this.state.data);
     this.setState({ errors });
+    if (Object.keys(errors).length == 0) {
+      this.props.submit(this.state.data);
+    }
   };
 
   validate = data => {
     const errors = {};
-    if (!Validator.isEmail(data.Email)) errors.email = "Invalid Email";
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid Email";
     if (!data.password) errors.password = "Can't be blank";
     return errors;
   };
@@ -35,27 +38,29 @@ class LoginForm extends React.Component {
     const { data, errors } = this.state;
     return (
       <Form onSubmit={this.onSubmit}>
-        <Form.Field>
+        <Form.Field error={errors.email}>
           <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
             name="email"
             placeholder="example@example.com"
-            value={this.state.data.email}
+            value={data.email}
             onChange={this.onChange}
           />
+          {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
-        <Form.Field>
+        <Form.Field error={!!errors.password}>
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             name="password"
             placeholder="Make it Secure."
-            value={this.state.data.password}
+            value={data.password}
             onChange={this.onChange}
           />
+          {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
